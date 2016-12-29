9cfaceebb00d039792765e8f640b1770eba071bf
Yan <alyan@amd.com>
2016-12-29 17:02:07 +0800

    Site updated: 2016-12-29 17:02:05

---
+++ js/totop.js
@@ -0,0 +1,3 @@
+// build time:Thu Dec 29 2016 17:01:55 GMT+0800 (China Standard Time)
+(function(o){var e=1e3;var t=o("#totop");var l=500;t.hide();o(window).scroll(function(){var l=o(document).scrollTop();if(l>e){o(t).stop().fadeTo(300,1)}else{o(t).stop().fadeTo(300,0)}});o(t).click(function(){o("html, body").animate({scrollTop:0},l);return false})})(jQuery);
+//rebuild by neat 
