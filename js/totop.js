4424e426d8d49b8eb13e71d2f51089dc8b5e0f5a
Alex <yjaelex@163.com>
2017-01-03 17:43:07 +0800

    Site updated: 2017-01-03 17:43:05

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 03 2017 17:39:14 GMT+0800 (China Standard Time)
+// build time:Tue Jan 03 2017 17:43:00 GMT+0800 (China Standard Time)
 (function(o){var e=1e3;var t=o("#totop");var l=500;t.hide();o(window).scroll(function(){var l=o(document).scrollTop();if(l>e){o(t).stop().fadeTo(300,1)}else{o(t).stop().fadeTo(300,0)}});o(t).click(function(){o("html, body").animate({scrollTop:0},l);return false})})(jQuery);
 //rebuild by neat 
